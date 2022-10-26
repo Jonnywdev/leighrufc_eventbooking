f8d3f7edf5aac9defb9bf2f7e290ce1550d0a377
Jonnywdev <jonnywhitt19@gmail.com>
2022-10-26 17:50:36 +0000

    Add style to all basic pages

--- roomhire/models.py
+++ roomhire/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    username = models.OneToOneField(User)
+    email = models.EmailField()
+    team = models.CharField(max_length=50)
+    phone = models.CharField(max_length=256, blank=True, null=True)
