ea3124df085d89398dce34557e9a4deed36975a3
Jonnywdev <jonnywhitt19@gmail.com>
2022-10-26 15:59:39 +0000

    initial setup complete

---
+++ leighrufc_eventbooking/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for leighrufc_eventbooking project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leighrufc_eventbooking.settings')
+
+application = get_asgi_application()
---
+++ roomhire/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoomhireConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'roomhire'
